1d50a1df3e8b614b38df9ae39eaaabab30e2a760
Ciffelia <mc.prince.0203@gmail.com>
2020-10-16 23:41:39 +0900

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -7,13 +7,3 @@ curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 for cfip in `cat /tmp/cf_ips`; do ufw allow from $cfip to any app 'WWW Secure' comment 'Cloudflare IP'; done
 
 ufw reload > /dev/null
-
-# OTHER EXAMPLE RULES
-# Retrict to port 80
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
-
-# Restrict to port 443
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
-
-# Restrict to ports 80 & 443
-#for cfip in `cat /tmp/cf_ips`; do ufw allow proto tcp from $cfip to any port 80,443 comment 'Cloudflare IP'; done

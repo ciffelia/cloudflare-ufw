ff4124f96d9f4cda07422025c0f0952bb4fc135d
Ciffelia <mc.prince.0203@gmail.com>
2020-10-16 23:42:07 +0900

    Update cloudflare-ufw.sh

--- cloudflare-ufw.sh
+++ cloudflare-ufw.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -eu
 
 curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
 curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
@@ -6,4 +8,6 @@ curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
 # Allow traffic from Cloudflare IPs to port 443
 for cfip in `cat /tmp/cf_ips`; do ufw allow from $cfip to any app 'WWW Secure' comment 'Cloudflare IP'; done
 
-ufw reload > /dev/null
+ufw reload
+
+rm /tmp/cf_ips
